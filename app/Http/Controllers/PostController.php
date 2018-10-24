d181ab1ff87add04e64993d75e8a185de061ed59
user_name <oliverelliotmoon@gmail.com>
2018-10-24 16:32:16 +0100

    PostController created, requests to retrieve posts and upload posts handled through this

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+
+  public function getPost(Request $request){
+
+  }
+
+  public function uploadPost(Request $request){
+
+  }
+}
