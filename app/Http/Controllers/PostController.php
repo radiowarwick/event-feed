c1e1f235a6dc496d2dc219173b8d84ff8ab37927
user_name <oliverelliotmoon@gmail.com>
2018-10-24 17:06:33 +0100

    added to PostController, small amount of code for uploading a post and getting the posts html

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,10 +8,14 @@ class PostController extends Controller
 {
 
   public function getPost(Request $request){
-
+    $post = Post::where('id',$request->input('id'));
+    return $post->generateHTML();
   }
 
   public function uploadPost(Request $request){
-
+    $type = $request->input('type');
+    $newPost = new Post;
+    $newPost->url = $request->input('url');
+    $newPost->type = $type;
   }
 }
